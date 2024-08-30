ee9f08252d0c1da5cf2f23fd0a2447c94a1fd08a
ALUMNO <ALUMNO@CC1PC15-16>
2024-08-30 15:02:05 -0600

    Agregar archivos de proyecto.

---
+++ Common/Attributes/AttributePeople.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Attributes
+{
+    // La clase 'AttributePeople' representa una persona con ciertos atributos.
+    public class AttributePeople
+    {
+        // Campo privado para el identificador de la persona.
+        private int iD;
+        // Campo privado para el nombre de la persona.
+        private string nombre;
+        // Campo privado para el apellido de la persona.
+        private string apellido;
+        // Campo privado para el sexo de la persona.
+        private string sexo;
+
+
+        /*El propósito general de estas propiedades
+         Encapsulación: Ayuda a mantener el principio de encapsulación al ocultar los campos privados y exponer solo lo necesario a través de las propiedades.
+         Control de Acceso: Permite agregar lógica adicional en los métodos get y set si se requiere, por ejemplo, validaciones o transformaciones de datos.*/
+        public int ID { get => iD; set => iD = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+    }
+}
---
+++ DataAccess/Connection/Connection_Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.Connection
+{
+    //Esta es la cadena de conexion
+    public class Connection_Database
+    {
+        private SqlConnection c = new SqlConnection("Data Source=CC1PC15-16;Initial Catalog=CRUD_N_CAPAS;Integrated Security=True;");
+        
+        public SqlConnection OpenConnection() //Abre la cadena de conexion
+        {
+            if (c.State == ConnectionState.Closed) c.Open();
+            return c;
+        }
+
+        public SqlConnection CloseConnection() //Cierra la cadena de conexion
+        {
+            if (c.State == ConnectionState.Open) c.Close();
+            return c;
+        }
+    }
+}
